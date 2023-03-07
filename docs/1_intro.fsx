29c682b3e31253c9fef30bbd657ebc5e613e6702
Johannes Sandgren <sorrowaureus@gmail.com>
2023-03-07 17:44:43 +0100

    Add documentation

---
+++ docs/1_intro.fsx
@@ -0,0 +1,18 @@
+(**
+---
+title: Introduction
+category: Documentation
+categoryindex: 1
+index: 1
+---
+*)
+
+
+(**
+Commented stuff becomes text
+
+*)
+
+let a: int = 0
+(*** include-fsi-output ***)
+
---
+++ docs/2_combinators.fsx
@@ -0,0 +1,15 @@
+(**
+---
+title: Some other things
+category: Documentation
+categoryindex: 1
+index: 2
+---
+*)
+
+(**
+
+Other stuff 
+
+asd
+*)
