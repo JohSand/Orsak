b6aa1da4acbf3af14de90194684a4c904aee0cc3
Johannes Sandgren <sorrowaureus@gmail.com>
2023-03-08 17:36:07 +0100

    Add documentation and update Readme

--- docs/1_intro.fsx
+++ docs/1_intro.fsx
@@ -6,13 +6,73 @@ categoryindex: 1
 index: 1
 ---
 *)
-
-
+(*** condition: prepare ***)
+#r "nuget: Microsoft.Extensions.Logging.Abstractions, 7.0.0"
+#I "../src/Orsak/bin/Release/net6.0"
+#r "Orsak.dll"
+open System.Threading.Tasks
+open Microsoft.Extensions.Logging
+open Microsoft.Extensions.Logging.Abstractions
 (**
-Commented stuff becomes text
+### Basic usage
 
+
+This is a library intended to make effectful code easier to manage. It allows the user to easily create and consume side-effects modelled as interfaces, by making use of computation expressions.
+
+Imagine having the interface
 *)
 
-let a: int = 0
-(*** include-fsi-output ***)
+type IButtonPusher =
+    abstract member PushButton: unit -> Task<unit>
 
+(**
+describing some business-critical capability. To integrate that with the effect framework, we create the following helpers:
+*)
+type IButtonPusherProvider =
+    abstract member ButtonPusher: IButtonPusher
+
+open Orsak
+
+module ButtonPusher =
+    let pushButton () =
+        Effect.Create(fun (provider: #IButtonPusherProvider) -> provider.ButtonPusher.PushButton ())
+
+(**
+Then, we can the write code like 
+*)
+
+let work () : Effect<#IButtonPusherProvider,unit,_> =
+    eff {
+        //code
+        do! ButtonPusher.pushButton ()
+        //more code
+    }
+
+(**
+Which can be run with some type which implements ``IButtonPusherProvider``.
+
+The big win with effects is that they compose. By using a flexible constraints the the effect creation function, we can
+simply add effects, which will constrain the effect runner type to implement both ``IButtonPusherProvider`` and ``ILoggerFactory``
+*)
+type ILoggerProvider =
+    abstract member Logger: ILogger
+
+let logInformation a =
+    Effect.Create(fun (provider: #ILoggerProvider) -> provider.Logger.Log(LogLevel.Information, a))
+
+let workThenLog() =
+    eff {
+        do! work ()
+        do! logInformation "Did the work."
+    }
+
+(** Then, in the composition root of the program, one could define: *)
+type EffectRunner(buttonPusher: IButtonPusher) =
+    interface ILoggerProvider with
+        member _.Logger = NullLogger.Instance
+    interface IButtonPusherProvider with
+        member _.ButtonPusher = buttonPusher
+(** And then run the effects: *)
+let runner: EffectRunner = EffectRunner(Unchecked.defaultof<_>)
+
+workThenLog().Run(runner)
--- docs/index.md
+++ docs/index.md
@@ -1,2 +1,11 @@
 Orsak
 ======
+
+Orsak is a library for function effects in F#, allowing pragmatic and composable modelling of side-effects through interfaces. It is inspired by [Zio](https://zio.dev), with the F# encoding similar to the approach described by Bartosz Sypytkowski [here](https://www.bartoszsypytkowski.com/dealing-with-complex-dependency-injection-in-f/).
+
+---
+
+This implementation makes use of resumable code to implement the computation expression builder for the lowest possible overhead. 
+The builder also has various overloads for ``Bind``, allowing for friction free interop with TaskLike, Async, Result<_,_> and more.
+It supports for, while, use, try-with and try-finally, and also applicative bind when the error-type supports ``+``.
+While the computation expression is the intended way of working with effects, the corresponding module should contain all the expected functions.
