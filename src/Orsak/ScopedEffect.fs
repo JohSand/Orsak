c4af6976046e75a20b73557926c4c982dbc383d7
Johannes Sandgren <sorrowaureus@gmail.com>
2023-04-15 14:18:52 +0200

    prototype of scoped and transactional effects

---
+++ src/Orsak/ScopedEffect.fs
@@ -0,0 +1,195 @@
+﻿[<Experimental("Experimental feature, API not stable")>]
+module Orsak.Scoped
+
+open Orsak
+open System.Threading
+open FSharp.Control
+open System.Threading.Tasks
+open System.Runtime.InteropServices
+open System
+open Microsoft.FSharp.Core.CompilerServices
+open Microsoft.FSharp.Core.CompilerServices.StateMachineHelpers
+open System.Data.Common
+
+
+type ScopedEffectCode<'Scope, 'Env, 'TOverall, 'T, 'Err> = EffectCode<'Env * 'Scope, 'TOverall, 'T, 'Err>
+
+#nowarn "57"
+#nowarn "3511"
+
+[<AbstractClass>]
+type ScopedEffectBuilder() =
+    inherit EffBuilderBase()
+
+//shenanigans to make overload resolution work nicely.
+[<AutoOpen>]
+module Extension =
+    type ScopedEffectBuilder with     
+    
+        member inline this.ReturnFrom<'Scope, 'Env, 'T, 'TOverall, 'Err>
+            (
+                eff: Effect<'Env, 'TOverall, 'Err>
+            ) : ScopedEffectCode<'Scope, 'Env, 'TOverall, 'TOverall, 'Err> =
+            ScopedEffectCode<'Scope, 'Env, 'TOverall, 'TOverall, 'Err>(fun sm ->
+                let (env, _) = sm.Data.Environment
+                let task = eff.Run env
+
+                if __useResumableCode then
+                    let mutable awaiter = task.GetAwaiter()
+
+                    let mutable __stack_fin = true
+
+                    if not awaiter.IsCompleted then
+                        let __stack_yield_fin = ResumableCode.Yield().Invoke(&sm)
+                        __stack_fin <- __stack_yield_fin
+
+                    if __stack_fin then
+                        let result = awaiter.GetResult()
+                        sm.Data.Result <- result
+                        true
+                    else
+                        sm.Data.MethodBuilder.AwaitUnsafeOnCompleted(&awaiter, &sm)
+                        false
+                else
+                    EffBuilder.BindDynamic(&sm, task, this.Return))
+
+        [<NoEagerConstraintApplication>]
+        member inline _.Bind<'Scope, 'Env, 'T, 'TOverall, 'TResult1, 'TResult2, 'Err>
+            (
+                eff: Effect<'Env, 'TResult1, 'Err>,
+                continuation: 'TResult1 -> ScopedEffectCode<'Scope, 'Env, 'TOverall, 'TResult2, 'Err>
+
+            ) : ScopedEffectCode<'Scope, 'Env, 'TOverall, 'TResult2, 'Err> =
+            ScopedEffectCode<'Scope, 'Env, 'TOverall, 'TResult2, 'Err>(fun sm ->
+                let (env, _) = sm.Data.Environment
+                let task = eff.Run env
+
+                if __useResumableCode then
+                    let mutable awaiter = task.GetAwaiter()
+
+                    let mutable __stack_fin = true
+
+                    if not awaiter.IsCompleted then
+                        let __stack_yield_fin = ResumableCode.Yield().Invoke(&sm)
+                        __stack_fin <- __stack_yield_fin
+
+                    if __stack_fin then
+                        let result = awaiter.GetResult()
+
+                        match result with
+                        | Ok result -> (continuation result).Invoke(&sm)
+                        | Error error ->
+                            sm.Data.Result <- Error error
+                            true
+                    else
+                        sm.Data.MethodBuilder.AwaitUnsafeOnCompleted(&awaiter, &sm)
+                        false
+                else
+                    EffBuilder.BindDynamic(&sm, task, continuation))
+
+type ScopeProvider<'Scope when 'Scope :> IAsyncDisposable> =
+    abstract member Scope: unit -> ValueTask<'Scope>
+
+type ScopedEffectBuilder<'Scope when 'Scope :> IAsyncDisposable>() =
+    inherit ScopedEffectBuilder()
+
+    member _.Run<'r, 'a, 'err when 'r :> ScopeProvider<'Scope>>
+        (effect: ScopedEffectCode<'Scope, 'r, 'a, 'a, 'err>)
+        : Effect<'r, 'a, 'err> =
+        mkEffect (fun env ->
+            vtask {
+                use! scope = (env :> ScopeProvider<'Scope>).Scope()
+
+                match! eff.Run(effect).Run((env, scope)) with
+                | Ok a -> return Ok a
+                | Error e -> return Error e
+            })
+
+type ExceptionHandler<'err> =
+    abstract member Handle: exn -> 'err
+
+type TransactionScope =
+    abstract member CommitAsync: unit -> Task
+    inherit IAsyncDisposable
+
+type TransactionScopeProvider<'Scope when 'Scope :> TransactionScope> = ScopeProvider<'Scope>
+
+type TransactionalEffectBuilder<'Scope when 'Scope :> TransactionScope>() =
+    inherit ScopedEffectBuilder()
+
+    member inline this.ReturnFrom<'Scope, 'Env, 'TOverall, 'Err>(eff: Effect<'Scope, 'TOverall, 'Err>)  : ScopedEffectCode<'Scope, 'Env, 'TOverall, 'TOverall, 'Err> = 
+        ScopedEffectCode<'Scope, 'Env, 'TOverall, 'TOverall, 'Err>(fun sm ->
+            let (_, scope) = sm.Data.Environment
+            let task = eff.Run scope
+
+            if __useResumableCode then
+                let mutable awaiter = task.GetAwaiter()
+
+                let mutable __stack_fin = true
+
+                if not awaiter.IsCompleted then
+                    let __stack_yield_fin = ResumableCode.Yield().Invoke(&sm)
+                    __stack_fin <- __stack_yield_fin
+
+                if __stack_fin then
+                    let result = awaiter.GetResult()
+                    sm.Data.Result <- result
+                    true
+
+                else
+                    sm.Data.MethodBuilder.AwaitUnsafeOnCompleted(&awaiter, &sm)
+                    false
+            else
+                EffBuilder.BindDynamic(&sm, task, this.Return))
+
+    [<NoEagerConstraintApplication>]
+    member inline _.Bind<'Env, 'T, 'TOverall, 'TResult1, 'TResult2, 'Err>
+        (
+            eff: Effect<'Scope, 'TResult1, 'Err>,
+            continuation: 'TResult1 -> ScopedEffectCode<'Scope, 'Env, 'TOverall, 'TResult2, 'Err>
+
+        ) : ScopedEffectCode<'Scope, 'Env, 'TOverall, 'TResult2, 'Err> =
+        ScopedEffectCode<'Scope, 'Env, 'TOverall, 'TResult2, 'Err>(fun sm ->
+            let (_, scope) = sm.Data.Environment
+            let task = eff.Run scope
+
+            if __useResumableCode then
+                let mutable awaiter = task.GetAwaiter()
+
+                let mutable __stack_fin = true
+
+                if not awaiter.IsCompleted then
+                    let __stack_yield_fin = ResumableCode.Yield().Invoke(&sm)
+                    __stack_fin <- __stack_yield_fin
+
+                if __stack_fin then
+                    let result = awaiter.GetResult()
+
+                    match result with
+                    | Ok result -> (continuation result).Invoke(&sm)
+                    | Error error ->
+                        sm.Data.Result <- Error error
+                        true
+                else
+                    sm.Data.MethodBuilder.AwaitUnsafeOnCompleted(&awaiter, &sm)
+                    false
+            else
+                EffBuilder.BindDynamic(&sm, task, continuation))
+
+    member _.Run<'r, 'a, 'err when 'r :> ExceptionHandler<'err> and 'r :> TransactionScopeProvider<'Scope>>
+        (effect: ScopedEffectCode<'Scope, 'r, 'a, 'a, 'err>)
+        : Effect<'r, 'a, 'err> =
+        mkEffect (fun env ->
+            vtask {
+                try
+                    use! scope = (env :> TransactionScopeProvider<'Scope>).Scope()
+
+                    match! eff.Run(effect).Run((env, scope)) with
+                    | Ok a ->
+                        do! scope.CommitAsync()
+                        return Ok a
+                    | Error e -> return Error e
+                with e ->
+                    let (err: 'err) = (env :> ExceptionHandler<'err>).Handle e
+                    return Error err
+            })
---
+++ tests/Orsak.Tests/ScopedTests.fs
@@ -0,0 +1,191 @@
+﻿namespace Orsak.Tests
+
+open System
+
+#nowarn "57"
+
+open System.Data.Common
+open System.Threading.Tasks
+open FSharp.Control
+open Microsoft.Data.Sqlite
+
+open Orsak
+open Orsak.Scoped
+open Xunit
+
+type IClock =
+    abstract member UtcNow: unit -> System.DateTimeOffset
+
+type IClockProvider =
+    abstract member Clock: IClock
+
+type Clock =
+    static member utcNow() =
+        Effect.Create(fun (provider: #IClockProvider) -> provider.Clock.UtcNow())
+
+
+
+type DbTransactional(tran: DbTransaction) =
+    member val Connection = tran.Connection
+
+    interface TransactionScope with
+        member this.CommitAsync() : Task = tran.CommitAsync()
+
+        member this.DisposeAsync() : ValueTask =
+            vtask {
+                do! tran.DisposeAsync()
+                do! this.Connection.DisposeAsync()
+            }
+
+type TestProvider() =
+    interface ExceptionHandler<string> with
+        member this.Handle e = e.ToString()
+
+    interface IClockProvider with
+        member this.Clock = { new IClock with member this.UtcNow() = System.DateTimeOffset.UtcNow }
+
+    interface TransactionScopeProvider<DbTransactional> with
+        member this.Scope() : ValueTask<DbTransactional> =
+            vtask {
+                let conn: DbConnection = new SqliteConnection("Data Source=test.db;Cache=Shared")
+                do! conn.OpenAsync()
+                let! tran = conn.BeginTransactionAsync()
+                return DbTransactional(tran)
+            }
+
+type Scoped() =
+    do
+        use conn = new SqliteConnection("Data Source=test.db;Cache=Shared")
+        conn.Open()
+        use tran = conn.BeginTransaction()
+        let command = conn.CreateCommand()
+        command.CommandText <- """
+                                CREATE TABLE TestData (
+	                                pkey INTEGER PRIMARY KEY AUTOINCREMENT,
+	                                data TEXT NOT NULL
+                                );"""
+        command.ExecuteNonQuery() |> ignore
+        tran.Commit()
+        ()
+
+
+    let commitEff = TransactionalEffectBuilder<DbTransactional>()
+
+    //Mostly explores what is possible with this api
+    //all the async usage is wasted with SQLite, but can be valuable with other providers.
+    let read () =
+        mkEffect (fun (trans: DbTransactional) ->
+            vtask {
+                let command = trans.Connection.CreateCommand()
+                command.CommandText <- """
+                    SELECT pkey, data FROM TestData
+                """
+                let list = ResizeArray()
+                try
+                    use! reader = command.ExecuteReaderAsync()
+                    while reader.ReadAsync() do
+                        let id = reader.GetInt32(0)
+                        let data = reader.GetString(1)
+                        list.Add((id, data))
+                    return Ok list
+                with e ->
+                    return Error (e.ToString())
+            })
+
+    let insert () =
+        mkEffect (fun (trans: DbTransactional) ->
+            vtask {
+                let command = trans.Connection.CreateCommand()
+                command.CommandText <- """
+                    INSERT INTO TestData (data) VALUES (@data)
+                """
+                let par = command.CreateParameter()
+                par.ParameterName <- "@data"
+                par.Value <- "test"
+                command.Parameters.Add par |> ignore
+                command.ExecuteNonQuery() |> ignore
+                return Ok ()
+            })
+
+    let run e = Effect.runOrFail<TestProvider, _, string> (TestProvider()) e
+    
+    let expectError (error: 'err) (e: Effect<_, _, 'err>) =
+        e
+        |> Effect.map (fun v -> failwith $"Got value %A{v} when expecting error %A{error}")
+        |> Effect.tryRecover (fun es ->
+            if es = error then
+                Ok()
+            else
+                failwith $"Got error %O{es} when expecting error %O{error}")
+
+    [<Fact>]
+    let ``Connection tests`` () =
+        commitEff {
+            let! list = read ()
+            Assert.Empty list
+            let! _now = Clock.utcNow ()
+            return 1
+        } |> run
+
+    [<Fact>]
+    let ``Connection tests 2`` () =
+        commitEff {
+            do! insert()
+            let! _now = Clock.utcNow ()
+            let! list = read ()
+            let value = Assert.Single(list)
+            Assert.Equal((1, "test"), value)
+            return 1
+        } |> run
+
+
+    [<Fact>]
+    let ``Connection tests 3`` () =
+        let insertWithError () =
+            commitEff {
+                do! insert()
+                let! list = read ()
+                let value = Assert.Single(list)
+                Assert.Equal((1, "test"), value)
+                return! Error "expected"
+            }
+
+        let read() =
+            commitEff {
+                return! read ()
+            }
+        eff {
+            do!
+                insertWithError ()
+                |> expectError "expected"
+
+            let! list = read()
+                
+            Assert.Empty list
+            return ()
+        }
+        |> run
+        
+
+    [<Fact>]
+    let ``Connection tests 4`` () =
+        commitEff {
+            do! insert()
+
+            let! list = read ()
+            let value = Assert.Single(list)
+            Assert.Equal((1, "test"), value)
+            return! Clock.utcNow ()
+        } |> run
+
+
+
+    interface IDisposable with
+        member this.Dispose(): unit = 
+            use conn = new SqliteConnection("Data Source=test.db;Cache=Shared")
+            conn.Open()
+            use tran = conn.BeginTransaction()
+            let command = conn.CreateCommand()
+            command.CommandText <- "DROP TABLE IF EXISTS TestData;"
+            command.ExecuteNonQuery() |> ignore
+            tran.Commit()
