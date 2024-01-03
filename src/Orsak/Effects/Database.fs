e64dfb7b6eaa50285405b9551bcec2b52cc72240
Johannes Sandgren <sorrowaureus@gmail.com>
2024-01-03 16:02:44 +0100

    Improve scoped effect API

--- src/Orsak/Effects/Database.fs
+++ src/Orsak/Effects/Database.fs
@@ -1,16 +1,11 @@
 ﻿namespace Orsak.Effects
 
-open System.Threading
-open System.Runtime.CompilerServices
-
 #nowarn "57"
 //#nowarn "3559"
 
-open System
 open System.Data.Common
 open System.Threading.Tasks
 open Orsak
-open Orsak.ScopeAware
 open Orsak.Scoped
 open FSharp.Control
 
@@ -26,54 +21,79 @@ type DbTransactional(tran: DbTransaction) =
             do! this.Connection.DisposeAsync()
         }
 
+/// <summary>
+/// Pure transaction effect.
+/// </summary>
 type Transaction<'a, 'err> = Effect<DbTransactional, 'a, 'err>
 
+/// <summary>
+/// Effects created by trxAware have this type.
+/// </summary>
 type EnlistedTransaction<'r, 'a, 'err> = Effect<'r * DbTransactional, 'a, 'err>
 
 namespace Orsak.Scoped
 
 open Orsak
 open Orsak.Effects
+open System.Threading
 open System.Threading.Tasks
 
 [<AutoOpen>]
 module Builder =
-    let commitEff = Orsak.Scoped.TransactionalEffectBuilder<DbTransactional>()
+    let commitEff = Orsak.Scoped.CompletableScopeCreatingEffectBuilder<DbTransactional>()
 
     let trxAware = Orsak.ScopeAware.ScopeAwareEffectBuilder<DbTransactional>()
 
+(*
+    Using scoped effects:
+*)
 
-//define some kind of provider that should be scoped
+//define some kind of Effect that should be scoped
 [<Interface>]
 type IProgressScope =
     abstract member Progress: System.IProgress<float>
-    inherit System.IAsyncDisposable
+    inherit Scoped
 
+//type alias for convini
+type IProgressScopeProvider = ScopeProvider<IProgressScope>
 
 //builder for your scope
-//once outside the scope, it is just a regular effect
 module Builder2 =
+    //using this to create an effect, will create an IProgressScope at the start of the effect, and it will be available
+    //throught the effect, and disposed at the end of the effect
+    //the result will be just a normal effect, that requires a IProgressScopeProvider
     let pEff = Orsak.Scoped.ScopeCreatingEffectBuilder<IProgressScope>()
 
+    //using this allows access to the IProgressScope created by a another effect
+    //expects to be bound in pEff
+    //if bound in a normal eff, the resulting Effect will be specialized as an enlisted progress effect
     let enlistP = Orsak.ScopeAware.ScopeAwareEffectBuilder<IProgressScope>()
 
+open Builder2
+
 module Progress =
-    open System.Threading
-    open Orsak
-    let report (t: float) =
-        Effect.Create(fun (x: #IProgressScope) -> x.Progress.Report(t))
+    //Pure progress effect, no other effects allowed.
+    type Progress<'err> = Effect<IProgressScope, unit, 'err>
 
-    open Builder2
+    //Function for calling our scoped effect
+    let report (t: float) : Progress<_> =
+        //by not making x generic, we restrict how report is called. (No #)
+        //it can now only really be called through pEff or enlistP
+        Effect.Create(fun (x: IProgressScope) -> x.Progress.Report(t))
 
-    let reportWhenDone r (e: Effect<'r,_,_>) = enlistP {
+    let reportWhenDone total (e: Effect<'r,_,_>) = enlistP {
         let! result = e
-        do! report r
+        do! report total
         return result
+    }
+
+    let reportWhenDone2 total (e: Effect<'r,_,_>) = eff {
+        let! a = reportWhenDone total e
+        return a
     }
 
     let whenAll (effects: Effect<'r,_,_> array) = pEff {
         let total = float effects.Length
-
         let! result =
             effects
             |> Array.map(reportWhenDone total)
@@ -99,38 +119,47 @@ module Progress =
         return ()
     }
 
-//type alias for convini
-type IProgressScopeProvider = ScopeProvider<IProgressScope>
+//Implementation of the Scoped Effect
+type ProgressScope() =
+    let mutable Calls = 0.
+    //unique progress per scope.
+    let p = new System.Progress<float>(fun _x -> ())
+    do
+        p.ProgressChanged.Add(fun total ->
+            Calls <- Calls + 1.
+            let pcnt = Calls / total
+            System.Console.WriteLine(pcnt)
+        )
+    interface IProgressScope with
+        member this.Progress = p
+        member this.DisposeAsync() = ValueTask()
 
+(* Mixing in other effects *)
 type GuidGenerator =
     abstract member NewGuid: unit -> System.Guid
 
 type GuidProvider =
     abstract member Gen: GuidGenerator
 
-type Runner() =
-    //here we do not need a separat entity, so we can combine with runner.
-    interface IProgressScope with
-        member this.DisposeAsync() = ValueTask()
-        member this.Progress = new System.Progress<float>(fun _x -> ())
-
-    interface IProgressScopeProvider with
-        member this.BeginScope() = ValueTask<_>(this :> IProgressScope)
-
-    interface GuidProvider with
-        member this.Gen = raise (System.NotImplementedException())
-
 module GuidGenerator =
     let newGuid () =
         Effect.Create(fun (p: #GuidProvider) -> p.Gen.NewGuid())
 
-module Test =
-    
-    open Orsak
 
-    let test2() =
-        [| GuidGenerator.newGuid () |]
-        |> Progress.whenAllBatch 10
+
+type Runner() =
+    interface IProgressScopeProvider with
+        member this.BeginScope() = ValueTask<IProgressScope>(ProgressScope())
+
+    interface GuidProvider with
+        member this.Gen = raise (System.NotImplementedException())
+
+module Test =
+    let test2() = eff {
+        return!
+            [| GuidGenerator.newGuid () |]
+            |> Progress.whenAllBatch 10
+    }
     
     let test() = task {
         match!
