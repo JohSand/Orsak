ad200fd520cab839fd13bf4c06d6aa0bc761c95e
Johannes Sandgren <sorrowaureus@gmail.com>
2025-04-18 12:32:47 +0200

    Initial

---
+++ src/Orsak.Myriad/Gen.fs
@@ -0,0 +1,151 @@
+﻿namespace Orsak.Myriad.Gen
+
+open Orsak
+
+type EffectContext<'a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k, 'l>
+    (a: 'a, b: 'b, c: 'c, d: 'd, e: 'e, f: 'f, g: 'g, h: 'h, i: 'i, j: 'j, k: 'k, l: 'l) =
+    member _.A = a
+    member _.B = b
+    member _.C = c
+    member _.D = d
+    member _.E = e
+    member _.F = f
+    member _.G = g
+    member _.H = h
+    member _.I = i
+    member _.J = j
+    member _.K = k
+    member _.L = l
+
+    member this.Create _ = this
+
+type EffectContext<'a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k>
+    (a: 'a, b: 'b, c: 'c, d: 'd, e: 'e, f: 'f, g: 'g, h: 'h, i: 'i, j: 'j, k: 'k) =
+    member _.A = a
+    member _.B = b
+    member _.C = c
+    member _.D = d
+    member _.E = e
+    member _.F = f
+    member _.G = g
+    member _.H = h
+    member _.I = i
+    member _.J = j
+    member _.K = k
+
+    member this.Create<'l>(l) =
+        EffectContext<'a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k, 'l>(a, b, c, d, e, f, g, h, i, j, k, l)
+
+type EffectContext<'a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j>
+    (a: 'a, b: 'b, c: 'c, d: 'd, e: 'e, f: 'f, g: 'g, h: 'h, i: 'i, j: 'j) =
+    member _.A = a
+    member _.B = b
+    member _.C = c
+    member _.D = d
+    member _.E = e
+    member _.F = f
+    member _.G = g
+    member _.H = h
+    member _.I = i
+    member _.J = j
+
+    member this.Create<'k>(k) =
+        EffectContext<'a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k>(a, b, c, d, e, f, g, h, i, j, k)
+
+type EffectContext<'a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i>(a: 'a, b: 'b, c: 'c, d: 'd, e: 'e, f: 'f, g: 'g, h: 'h, i: 'i) =
+    member _.A = a
+    member _.B = b
+    member _.C = c
+    member _.D = d
+    member _.E = e
+    member _.F = f
+    member _.G = g
+    member _.H = h
+    member _.I = i
+
+    member this.Create<'j>(j) =
+        EffectContext<'a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j>(a, b, c, d, e, f, g, h, i, j)
+
+type EffectContext<'a, 'b, 'c, 'd, 'e, 'f, 'g, 'h>(a: 'a, b: 'b, c: 'c, d: 'd, e: 'e, f: 'f, g: 'g, h: 'h) =
+    member _.A = a
+    member _.B = b
+    member _.C = c
+    member _.D = d
+    member _.E = e
+    member _.F = f
+    member _.G = g
+    member _.H = h
+
+    member this.Create<'i>(i) =
+        EffectContext<'a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i>(a, b, c, d, e, f, g, h, i)
+
+type EffectContext<'a, 'b, 'c, 'd, 'e, 'f, 'g>(a: 'a, b: 'b, c: 'c, d: 'd, e: 'e, f: 'f, g: 'g) =
+    member _.A = a
+    member _.B = b
+    member _.C = c
+    member _.D = d
+    member _.E = e
+    member _.F = f
+    member _.G = g
+
+    member this.Create<'h>(h) =
+        EffectContext<'a, 'b, 'c, 'd, 'e, 'f, 'g, 'h>(a, b, c, d, e, f, g, h)
+
+type EffectContext<'a, 'b, 'c, 'd, 'e, 'f>(a: 'a, b: 'b, c: 'c, d: 'd, e: 'e, f: 'f) =
+    member _.A = a
+    member _.B = b
+    member _.C = c
+    member _.D = d
+    member _.E = e
+    member _.F = f
+
+    member this.Create<'g>(g) =
+        EffectContext<'a, 'b, 'c, 'd, 'e, 'f, 'g>(a, b, c, d, e, f, g)
+
+type EffectContext<'a, 'b, 'c, 'd, 'e>(a: 'a, b: 'b, c: 'c, d: 'd, e: 'e) =
+    member _.A = a
+    member _.B = b
+    member _.C = c
+    member _.D = d
+    member _.E = e
+
+    member this.Create<'f>(f) =
+        EffectContext<'a, 'b, 'c, 'd, 'e, 'f>(a, b, c, d, e, f)
+
+type EffectContext<'a, 'b, 'c, 'd>(a: 'a, b: 'b, c: 'c, d: 'd) =
+    member _.A = a
+    member _.B = b
+    member _.C = c
+    member _.D = d
+
+    member this.Create<'e>(e) =
+        EffectContext<'a, 'b, 'c, 'd, 'e>(a, b, c, d, e)
+
+type EffectContext<'a, 'b, 'c>(a: 'a, b: 'b, c: 'c) =
+    member _.A = a
+    member _.B = b
+    member _.C = c
+
+    member this.Create<'d>(d) =
+        EffectContext<'a, 'b, 'c, 'd>(a, b, c, d)
+
+type EffectContext<'a, 'b>(a: 'a, b: 'b) =
+    member _.A = a
+    member _.B = b
+    member this.Create<'c>(c) = EffectContext<'a, 'b, 'c>(a, b, c)
+
+type EffectContext<'a>(a: 'a) =
+    member _.A = a
+    member this.Create<'b>(b) = EffectContext<'a, 'b>(a, b)
+
+[<Struct>]
+type EffectContext =
+    member this.Create<'b>(b) = EffectContext<'b>(b)
+
+type GenContext<'a, 'b, 'c when 'a: (member Create: 'b -> 'c)> = 'a
+
+module Runner =
+    let createFrom (a: 't) =
+        { new IProvide<'t> with
+            member _.Effect = a
+        }
---
+++ tests/Orsak.Tests/GenWriterTests.fs
@@ -0,0 +1,229 @@
+﻿module Orsak.Tests.GenWriterTests
+
+
+open Orsak
+open Orsak.Myriad
+open Orsak.Myriad
+open Orsak.Myriad.Gen
+open Xunit
+open System.Text
+open System.Threading
+open Microsoft.Extensions.Caching.Memory
+
+[<Fact>]
+let writeCorrectParmeters () =
+    let sb = StringBuilder().ToIndentingBuilder()
+    do Writer.writeTypeParameters "Test" 5 1 sb
+    let result = sb.ToString()
+    Assert.Equal("<Test, 'b, 'c, 'd, 'e>", result)
+    ()
+
+
+[<Fact>]
+let correctWriteExtractorMembers () =
+    let sb = StringBuilder().ToIndentingBuilder()
+    do Writer.writeExtractorMembers "RandomGeneratorExtractor" "IRandomGenerator" 5 sb
+    let result = sb.ToString()
+
+    let expected =
+        "\
+static member Extract(_: RandomGeneratorExtractor, e: EffectContext<IRandomGenerator, 'b, 'c, 'd, 'e>) = e.A
+static member Extract(_: RandomGeneratorExtractor, e: EffectContext<'a, IRandomGenerator, 'c, 'd, 'e>) = e.B
+static member Extract(_: RandomGeneratorExtractor, e: EffectContext<'a, 'b, IRandomGenerator, 'd, 'e>) = e.C
+static member Extract(_: RandomGeneratorExtractor, e: EffectContext<'a, 'b, 'c, IRandomGenerator, 'e>) = e.D
+static member Extract(_: RandomGeneratorExtractor, e: EffectContext<'a, 'b, 'c, 'd, IRandomGenerator>) = e.E
+"
+
+    Assert.Equal(expected, result, ignoreLineEndingDifferences = true)
+    ()
+
+[<Fact>]
+let correctWriteExtractor () =
+    let sb = StringBuilder().ToIndentingBuilder()
+    do Writer.writeExtractor "IRandomGenerator" [| 2; 5 |] sb
+    let result = sb.ToString()
+
+    let expected =
+        "\
+type RandomGeneratorExtractor =
+    static member Extract(_: RandomGeneratorExtractor, e: EffectContext<IRandomGenerator, 'b>) = e.A
+    static member Extract(_: RandomGeneratorExtractor, e: EffectContext<'a, IRandomGenerator>) = e.B
+    static member Extract(_: RandomGeneratorExtractor, e: EffectContext<IRandomGenerator, 'b, 'c, 'd, 'e>) = e.A
+    static member Extract(_: RandomGeneratorExtractor, e: EffectContext<'a, IRandomGenerator, 'c, 'd, 'e>) = e.B
+    static member Extract(_: RandomGeneratorExtractor, e: EffectContext<'a, 'b, IRandomGenerator, 'd, 'e>) = e.C
+    static member Extract(_: RandomGeneratorExtractor, e: EffectContext<'a, 'b, 'c, IRandomGenerator, 'e>) = e.D
+    static member Extract(_: RandomGeneratorExtractor, e: EffectContext<'a, 'b, 'c, 'd, IRandomGenerator>) = e.E
+"
+
+    Assert.Equal(expected, result, ignoreLineEndingDifferences = true)
+
+[<Fact>]
+let testTrimI () =
+    let trimmed = Writer.trimI "IMemoryCache"
+    Assert.Equal("MemoryCache", trimmed)
+
+
+[<Fact>]
+let testTrimI2 () =
+    let trimmed = Writer.trimI "MemoryCache"
+    Assert.Equal("MemoryCache", trimmed)
+
+[<Fact>]
+let testWriteRun () =
+    let sb = StringBuilder().ToIndentingBuilder()
+    Writer.writeRunMethod 0 0 [| "CancellationTokenSource"; "IMemoryCache"; "IRandomGenerator" |] sb
+    let x = sb.ToString()
+
+    let expected =
+        "\
+member inline _.Run(a) =
+    match a with
+    | ExtractCancellationTokenSource(a) & ExtractMemoryCache(b) & ExtractRandomGenerator(c) -> {
+        CancellationTokenSource = a
+        MemoryCache = b
+        RandomGenerator = c
+    }
+"
+
+    Assert.Equal(expected, x, ignoreLineEndingDifferences = true)
+
+[<Fact>]
+let testWriteRun2 () =
+    let sb = StringBuilder().ToIndentingBuilder()
+    Writer.writeRunMethod 0 2 [| "CancellationTokenSource"; "IMemoryCache"; "IRandomGenerator" |] sb
+    let x = sb.ToString()
+
+    let expected =
+        "\
+member inline _.Run(a, [<Optional>]_a: byte, [<Optional>]_b: sbyte) =
+    match a with
+    | ExtractCancellationTokenSource(a) & ExtractMemoryCache(b) & ExtractRandomGenerator(c) -> {
+        CancellationTokenSource = a
+        MemoryCache = b
+        RandomGenerator = c
+    }
+"
+
+    Assert.Equal(expected, x, ignoreLineEndingDifferences = true)
+
+[<Fact>]
+let testWriteRun4 () =
+    let sb = StringBuilder().ToIndentingBuilder()
+    Writer.writeRunMethod 10 5 [| "CancellationTokenSource"; "IMemoryCache"; "IRandomGenerator" |] sb
+    let x = sb.ToString()
+
+    let expected =
+        "\
+member inline _.Run(a, [<Optional>]_a: double, [<Optional>]_b: char, [<Optional>]_c: bool, [<Optional>]_d: byte, [<Optional>]_e: sbyte) =
+    match a with
+    | ExtractCancellationTokenSource(a) & ExtractMemoryCache(b) & ExtractRandomGenerator(c) -> {
+        CancellationTokenSource = a
+        MemoryCache = b
+        RandomGenerator = c
+    }
+"
+
+    Assert.Equal(expected, x, ignoreLineEndingDifferences = true)
+
+[<Fact>]
+let testWriteRun3 () =
+    let sb = StringBuilder().ToIndentingBuilder()
+
+    let arity = {
+        requiredTieBreakers = 2
+        runners = [|
+            { effects = [| "CancellationTokenSource"; "IMemoryCache" |]; name = "todo" }
+            { effects = [| "IMemoryCache"; "IRandomGenerator" |]; name = "todo" }
+        |]
+        arity = 2
+    }
+
+    Writer.writeRunMethods arity sb
+    let x = sb.ToString()
+
+    let expected =
+        "\
+member inline _.Run(a, [<Optional>]_a: byte, [<Optional>]_b: sbyte) =
+    match a with
+    | ExtractCancellationTokenSource(a) & ExtractMemoryCache(b) -> {
+        CancellationTokenSource = a
+        MemoryCache = b
+    }
+member inline _.Run(a, [<Optional>]_a: sbyte, [<Optional>]_b: int16) =
+    match a with
+    | ExtractMemoryCache(a) & ExtractRandomGenerator(b) -> {
+        MemoryCache = a
+        RandomGenerator = b
+    }
+"
+
+    Assert.Equal(expected, x, ignoreLineEndingDifferences = true)
+
+
+[<Fact>]
+let testWriteRunner () =
+    let sb = StringBuilder().ToIndentingBuilder()
+    Writer.writeRunner { name = "Runner0"; effects =  [| "CancellationTokenSource"; "IMemoryCache"; "IRandomGenerator" |] } sb
+    let x = sb.ToString()
+
+    let expected =
+        "\
+type Runner0 = {
+    CancellationTokenSource: CancellationTokenSource
+    MemoryCache: IMemoryCache
+    RandomGenerator: IRandomGenerator
+} with
+    interface IProvide<CancellationTokenSource> with
+        member this.Effect = this.CancellationTokenSource
+    interface IProvide<IMemoryCache> with
+        member this.Effect = this.MemoryCache
+    interface IProvide<IRandomGenerator> with
+        member this.Effect = this.RandomGenerator
+"
+
+    Assert.Equal(expected, x, ignoreLineEndingDifferences = true)
+
+
+[<Fact>]
+let createLayerTests () =
+    let sut = [
+        [| "CancellationTokenSource"; "IMemoryCache" |]
+        [| "CancellationTokenSource"; "IMemoryCache"; "IRandomGenerator" |]
+        [| "IMemoryCache"; "IRandomGenerator" |]
+
+    ]
+
+    let result = Writer.orderLayers sut
+
+    let expected: RunnerArityCfg array = [|
+        {
+            runners = [|
+                { effects = [| "CancellationTokenSource"; "IMemoryCache"; "IRandomGenerator" |]; name = "Runner3W0" }
+            |]
+            requiredTieBreakers = 0
+            arity = 3
+        }
+        {
+            runners = [|
+                { effects = [| "CancellationTokenSource"; "IMemoryCache" |]; name = "Runner2W0" }
+                { effects = [| "IMemoryCache"; "IRandomGenerator" |]; name = "Runner2W1" }
+            |]
+            requiredTieBreakers = 1
+            arity = 2
+        }
+    |]
+
+    Assert.Equal<RunnerArityCfg>(expected, result)
+
+[<Fact>]
+let writeExtractPatternTests () =
+    let sb = StringBuilder().ToIndentingBuilder()
+    Writer.writeExtractPattern "IMemoryCache" sb
+    let result = sb.ToString()
+    let expected =
+        "\
+let inline (|ExtractMemoryCache|) a : IMemoryCache =
+    extract Unchecked.defaultof<MemoryCacheExtractor> a
+"
+    Assert.Equal(expected, result, ignoreLineEndingDifferences = true)
+    ()
