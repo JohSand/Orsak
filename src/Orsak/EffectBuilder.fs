335e58a04d2658e112ee6237bb39d878f1b7534e
Johannes Sandgren <sorrowaureus@gmail.com>
2025-04-19 10:25:30 +0200

    Nowarn build for 6.0 target

--- src/Orsak/EffectBuilder.fs
+++ src/Orsak/EffectBuilder.fs
@@ -3742,8 +3742,4 @@ type Effect<'R, 'T, 'E> with
     (* applicative *)
     ///Implements Apply on the effect, making it an applicative
     static member inline (<*>)(f: Effect<'r, 'b -> 'a, 'e>, e: Effect<'r, 'b, 'e>) =
-        eff {
-            let! fn = f
-            and! b = e
-            return fn b
-        }
+        Effect.ap f e
