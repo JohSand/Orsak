e4ff807ffcf38dd643c824fc50242e1493bb0949
JohSand <johannesosandgren@gmail.com>
2026-03-23 22:54:08 +0100

    Fix for generic and named function parameters for GenEffect

---
+++ tests/Orsak.Tests/TestDataVerified/Effects.Myriad.08.verified.fsx
@@ -0,0 +1,33 @@
+﻿namespace Tmp
+#r "../bin/Debug/net9.0/Orsak.dll"
+#r "../bin/Debug/net9.0/Orsak.Myriad.dll"
+
+open System
+open System.Threading.Tasks
+open Orsak
+open Orsak.Myriad
+
+[<GenEffects>]
+type IPersistenceService =
+    abstract Persistence: int -> Task
+
+    abstract Test1: int -> Task
+
+    abstract Test2: int -> Task
+
+    abstract Test3: int -> Task
+
+namespace Tmp
+open Orsak
+type IPersistenceServiceProvider =
+    abstract Effect: IPersistenceService
+
+module PersistenceService =
+    let persistence a =
+        Effect.Create(fun (er: #IPersistenceServiceProvider) -> er.Effect.Persistence a)
+    let test1 a =
+        Effect.Create(fun (er: #IPersistenceServiceProvider) -> er.Effect.Test1 a)
+    let test2 a =
+        Effect.Create(fun (er: #IPersistenceServiceProvider) -> er.Effect.Test2 a)
+    let test3 a =
+        Effect.Create(fun (er: #IPersistenceServiceProvider) -> er.Effect.Test3 a)
